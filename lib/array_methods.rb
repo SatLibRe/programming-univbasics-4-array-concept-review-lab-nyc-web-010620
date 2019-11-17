4860523ae22eb9826b061fabb6436c6e5e8f0fff
Miles Higbie <miles.higbie@gmail.com>
2019-11-17 23:16:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   i = 0 
   while array.length > i do
     if array[i]  == value_to_find
-    return value_to_find - 1 
+    return value_to_find[i] 
     end
     i += 1
   end 
